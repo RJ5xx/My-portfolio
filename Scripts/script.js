e5c3e36c8316cd141309e016b6928f6dcd100225
Cresent <98427299+Cresent25@users.noreply.github.com>
2022-07-24 15:06:45 +0100

    🚀 Removed some content

---
+++ Scripts/script.js
@@ -0,0 +1,13 @@
+// Reviews Section - Swiper.js
+var swiper = new Swiper(".reviews .content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
