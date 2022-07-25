bc39f88fa33ff2e86bcb66a5f988dbcf5cc750d1
Cresent <98427299+Cresent25@users.noreply.github.com>
2022-07-25 20:07:07 +0100

    🐝 Added an alert box using Sweet Alert

---
+++ Scripts/script.js
@@ -0,0 +1,21 @@
+swal("It\'s my birthday on the 29th! 🎂", {
+    buttons: {
+        congratulate: {
+            text: "Congratulate me! 🎉",
+            value: "congratulate",
+        },
+        donate: {
+            text: "Donate to me! 💸"
+        }
+    },
+}).then((value) => {
+    switch (value) {
+        case "congratulate":
+            swal("Cool!", "Thanks for congratulating me!", "success");
+            break;
+
+        case "donate":
+            swal("Here is a link for you to buy me some cake! https://paypal.me/neutral75");
+            break;
+    }
+});
