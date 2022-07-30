ec49991593f847930438d6f6d5970245d7086572
Cresent <98427299+Cresent25@users.noreply.github.com>
2022-07-30 18:34:07 +0100

    🎳 Removed sweet alert features

--- Scripts/script.js
+++ Scripts/script.js
@@ -11,32 +11,3 @@ VANTA.FOG({
     baseColor: 0xffffff,
     speed: 1.20
 });
-
-swal("It\'s my birthday on the 29th! 🎂", {
-    buttons: {
-        congratulate: {
-            text: "Congratulate me! 🎉",
-        },
-        donate: {
-            text: "Donate to me! 💸"
-        }
-    },
-}).then((value) => {
-    switch (value) {
-        case "congratulate":
-            swal({
-                icon: "success",
-                title: "Cool! 🎉",
-                text: "Thanks for congratulating me!"
-            });
-            break;
-
-        case "donate":
-            swal({
-                icon: "success",
-                title: "Thanks! 💸",
-                text: "Donate to to me from the link below! https://paypal.me/neutral75"
-            });
-            break;
-    }
-});
